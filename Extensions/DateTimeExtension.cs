7faf1c2e0263f65cee1f95dbaea70dc66ca57d3d
lekyvong09 <lekyvong09@gmail>
2021-02-14 16:54:58 +0700

    Add calculate age into DateTime data type

---
+++ Extensions/DateTimeExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        // Keyword "this" allow to call the static method in any DateTime props
+        public static int CalculateAge(this DateTime dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dob.Year;
+            if (dob.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
