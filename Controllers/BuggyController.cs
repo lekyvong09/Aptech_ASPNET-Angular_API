a372ca09f9299c2c0deff33afeb00c7949afbff8
lekyvong09 <lekyvong09@gmail>
2021-02-14 16:07:48 +0700

    * Photo.cs: * AppUser.cs: * BuggyController.cs:
    * appsettings.Development.json: Extending User Entity

--- Controllers/BuggyController.cs
+++ Controllers/BuggyController.cs
@@ -21,7 +21,6 @@ namespace API.Controllers
             return "secret text";
         }
 
-        [Authorize]
         [HttpGet("not-found")]
         public ActionResult<AppUser> GetNotFound()
         {
@@ -32,7 +31,6 @@ namespace API.Controllers
             return Ok(thing);
         }
 
-        [Authorize]
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError()
         {
@@ -41,7 +39,6 @@ namespace API.Controllers
                 return thingToReturn;   
         }
 
-        [Authorize]
         [HttpGet("bad-request")]
         public ActionResult<string> GetBadRequest()
         {
