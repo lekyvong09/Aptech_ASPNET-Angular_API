8dd125b0282b0f2d7e04d331df05b20e19fbbfa5
lekyvong09 <lekyvong09@gmail>
2021-02-28 17:04:30 +0700

    add like controller

--- Controllers/LikesController.cs
+++ Controllers/LikesController.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -18,11 +22,43 @@ namespace API.Controllers
             _likesRepository = likesRepository;
         }
 
-        //[HttpPost("{username}")]
-        //public async Task<ActionResult> AddLike(string username)
-        //{
-        //    return null;
-        //}
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string username)
+        {
+            var sourceUserId = Int32.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            var likedUser = await _userRepository.GetUserByUsernameAsync(username);
+            var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+
+            if (likedUser == null) return NotFound();
+
+            if (sourceUser.Username == username) return BadRequest("You cannot like yourself");
+
+            var userLike = await _likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+            if (userLike != null) return BadRequest("You already like this user");
+
+            userLike = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                LikedUserId = likedUser.Id
+            };
+
+            sourceUser.LikedUsers.Add(userLike);
+
+            if (await _userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Failed to like user");
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+        {
+            var userId = Int32.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            var users = await _likesRepository.GetUserLikes(predicate, userId);
+
+            return Ok(users);
+        }
 
     }
 }
