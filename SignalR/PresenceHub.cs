04c9f01efc64b944313eb233d24a8bda3312b538
lekyvong09 <lekyvong09@gmail>
2021-03-11 16:02:36 +0700

    SignalR - create a global caching to keep user connection state on Application Server

--- SignalR/PresenceHub.cs
+++ SignalR/PresenceHub.cs
@@ -9,17 +9,34 @@ namespace API.SignalR
     [Authorize]
     public class PresenceHub : Hub
     {
+        private readonly PresenceTracker _tracker;
+
+        public PresenceHub(PresenceTracker presenceTracker)
+        {
+            _tracker = presenceTracker;
+        }
+
         public override async Task OnConnectedAsync()
         {
             var username = Context.User.FindFirst(ClaimTypes.Name)?.Value;
+
+            await _tracker.UserConnected(username, Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOnline", username);
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
         }
 
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
             var username = Context.User.FindFirst(ClaimTypes.Name)?.Value;
+
+            await _tracker.UserConnected(username, Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOffline", username);
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
 
             await base.OnDisconnectedAsync(exception);
         }
